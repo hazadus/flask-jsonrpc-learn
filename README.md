93fe1e51d613fd8fce69fdbb66839f99e4c1b681
hazadus <hazadus7@gmail.com>
2024-02-26 12:48:29 +0300

    feat: Basic calculator API implemented.

--- README.md
+++ README.md
@@ -4,6 +4,38 @@ JSON-RPC demo app.
 
 Browsable API available at: http://127.0.0.1:5000/api/browse/
 
+## Sample requests
+
+```bash
+curl -i -X POST  -H "Content-Type: application/json; indent=4" \
+  -d '{
+    "jsonrpc": "2.0",
+    "method": "App.sum",
+    "params": {
+        "x": 1,
+        "y": 2
+    },
+    "id": "1"
+  }' http://127.0.0.1:5000/api
+```
+
+Sample response:
+
+```http request
+HTTP/1.1 200 OK
+Server: Werkzeug/3.0.1 Python/3.12.1
+Date: Mon, 26 Feb 2024 09:30:59 GMT
+Content-Type: application/json
+Content-Length: 51
+Connection: close
+
+{
+  "id": "1",
+  "jsonrpc": "2.0",
+  "result": 3
+}
+```
+
 ## References
 
 - https://www.jsonrpc.org/specification
--- app.py
+++ app.py
@@ -1,13 +1,39 @@
 from flask import Flask
 from flask_jsonrpc import JSONRPC
+from flask_jsonrpc.exceptions import JSONRPCError
 
 app = Flask("application")
 jsonrpc = JSONRPC(app, "/api", enable_web_browsable_api=True)
 
 
-@jsonrpc.method("App.index")
-def index() -> str:
-    return "Welcome to Flask JSON-RPC"
+@jsonrpc.method("App.sum")
+def sum_(x: int, y: int) -> int:
+    """Return sum of x and y."""
+    return x + y
+
+
+@jsonrpc.method("App.subtract")
+def subtract(x: int, y: int) -> int:
+    """Return difference of x and y."""
+    return x - y
+
+
+@jsonrpc.method("App.multiply")
+def multiply(x: int, y: int) -> int:
+    """Return x multiplied by y."""
+    return x * y
+
+
+@jsonrpc.method("App.divide")
+def divide(x: int, y: int) -> float:
+    """Return x divided by y."""
+    if y == 0:
+        error = JSONRPCError()
+        error.code = -32602  # "Invalid method parameter(s)."
+        error.status_code = 400
+        error.data = {"message": "Can't divide by zero!"}
+        raise error
+    return x / y
 
 
 if __name__ == "__main__":
